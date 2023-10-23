609eb80e328004cbe44dd1c9e853dcbf0844790c
Javed Chauhan <chauhanjaved@gmail.com>
2023-10-23 13:38:45 +0530

    Add NextAuth google provider

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -83,7 +83,7 @@ const Nav = () => {
               />
             {toggleDropdown && (
                <div className='dropdown'>
-                                <Link
+                <Link
                   href='/profile'
                   className='dropdown_link'
                   onClick={() => setToggleDropdown(false)}
